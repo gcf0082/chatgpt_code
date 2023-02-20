b3c13add6ea300c8dc43e87608b1b9f80f2f4614
gcf0082 <13510989573@sina.cn>
2023-02-20 22:22:04 +0800

    Update 服务端代码.java

--- java/用java写一个ntp服务端和客户端，客户端可以获取与服务端的时间差值/服务端代码.java
+++ java/用java写一个ntp服务端和客户端，客户端可以获取与服务端的时间差值/服务端代码.java
@@ -1,52 +1,46 @@
-import java.net.*;
-import java.io.*;
-import java.util.*;
+import org.apache.commons.net.ntp.NtpV3Packet;
+import org.apache.commons.net.ntp.NtpV3Impl;
+import org.apache.commons.net.ntp.NtpUtils;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 
 public class NTPServer {
-    private static final long EPOCH_DIFF = 2208988800L;
+    public static void main(String[] args) throws Exception {
+        // 设置监听端口
+        int port = 123;
 
-    public static void main(String[] args) {
-        DatagramSocket socket = null;
+        // 创建 DatagramSocket 对象
+        DatagramSocket socket = new DatagramSocket(port);
+        byte[] buffer = new byte[48];
+        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
 
-        try {
-            socket = new DatagramSocket(123);
-            byte[] buffer = new byte[48];
+        while (true) {
+            // 接收请求
+            socket.receive(packet);
 
-            while (true) {
-                DatagramPacket request = new DatagramPacket(buffer, buffer.length);
-                socket.receive(request);
+            // 解析请求
+            NtpV3Impl ntpRequest = new NtpV3Impl();
+            ntpRequest.setDatagramPacket(packet);
+            ntpRequest.parse();
 
-                long receiveTime = System.currentTimeMillis() / 1000 + EPOCH_DIFF;
-                long transmitTime = System.currentTimeMillis() / 1000 + EPOCH_DIFF;
+            // 处理请求
+            NtpV3Impl ntpResponse = new NtpV3Impl();
+            ntpResponse.setMode(NtpV3Packet.MODE_SERVER);
+            ntpResponse.setStratum(1);
+            ntpResponse.setVersion(ntpRequest.getVersion());
+            ntpResponse.setReferenceTime(ntpRequest.getTransmitTimeStamp());
+            ntpResponse.setOriginateTimeStamp(ntpRequest.getTransmitTimeStamp());
+            ntpResponse.setReceiveTimeStamp(System.currentTimeMillis());
+            ntpResponse.setTransmitTimeStamp(System.currentTimeMillis());
 
-                buffer[0] = (byte) 0x1B;
-                buffer[1] = (byte) 0x00;
-                buffer[2] = (byte) 0x00;
-                buffer[3] = (byte) 0x00;
-                buffer[4] = (byte) 0x00;
-                buffer[5] = (byte) 0x00;
-                buffer[6] = (byte) 0x00;
-                buffer[7] = (byte) 0x00;
-                buffer[8] = (byte) ((transmitTime >> 24) & 0xFF);
-                buffer[9] = (byte) ((transmitTime >> 16) & 0xFF);
-                buffer[10] = (byte) ((transmitTime >> 8) & 0xFF);
-                buffer[11] = (byte) (transmitTime & 0xFF);
-                buffer[12] = (byte) ((receiveTime >> 24) & 0xFF);
-                buffer[13] = (byte) ((receiveTime >> 16) & 0xFF);
-                buffer[14] = (byte) ((receiveTime >> 8) & 0xFF);
-                buffer[15] = (byte) (receiveTime & 0xFF);
-
-                DatagramPacket response = new DatagramPacket(buffer, buffer.length, request.getAddress(), request.getPort());
-                socket.send(response);
-            }
-        } catch (SocketException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (socket != null) {
-                socket.close();
-            }
+            // 发送响应
+            byte[] data = ntpResponse.toByteArray();
+            InetAddress clientAddress = packet.getAddress();
+            int clientPort = packet.getPort();
+            packet = new DatagramPacket(data, data.length, clientAddress, clientPort);
+            socket.send(packet);
         }
     }
 }
