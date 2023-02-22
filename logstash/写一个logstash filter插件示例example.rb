2eb4d8b7cae23ab73dca196691f898f2677cba6a
gcf0082 <13510989573@sina.cn>
2023-02-22 10:06:45 +0800

    Create 写一个logstash filter插件示例example.rb

---
+++ logstash/写一个logstash filter插件示例example.rb
@@ -0,0 +1,13 @@
+require 'logstash/filters/base'
+
+class LogStash::Filters::Example < LogStash::Filters::Base
+  config_name 'example'
+
+  def filter(event)
+    return unless filter?(event)
+
+    event.set('message', event.get('message').downcase)
+
+    filter_matched(event)
+  end
+end
