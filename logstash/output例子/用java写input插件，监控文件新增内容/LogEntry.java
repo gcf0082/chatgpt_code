90369cf5cefae98ee2c7bd2771cc4125c2e63acf
gcf0082 <13510989573@sina.cn>
2023-02-22 11:13:15 +0800

    Create LogEntry.java

---
+++ logstash/output例子/用java写input插件，监控文件新增内容/LogEntry.java
@@ -0,0 +1,22 @@
+import java.time.LocalDateTime;
+
+public class LogEntry {
+    private LocalDateTime timestamp;
+    private String message;
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
