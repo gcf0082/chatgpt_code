b91470f2b1a882149c7391d967291f26c45aacba
gcf0082 <13510989573@sina.cn>
2023-02-23 16:35:36 +0800

    Update 使用广度优先获取向下的边.java

--- java/java实现图数据库2/使用广度优先获取向下的边.java
+++ java/java实现图数据库2/使用广度优先获取向下的边.java
@@ -1,1 +1,20 @@
-
+public List<Edge> getEdgesDownstream(Node node, Graph graph) {
+    List<Edge> result = new ArrayList<>();
+    Queue<Node> queue = new LinkedList<>();
+    Set<Node> visited = new HashSet<>();
+    queue.offer(node);
+    visited.add(node);
+    while (!queue.isEmpty()) {
+        Node current = queue.poll();
+        List<Edge> edges = graph.getEdgesByNode(current);
+        for (Edge edge : edges) {
+            result.add(edge);
+            Node next = edge.getTo();
+            if (!visited.contains(next)) {
+                visited.add(next);
+                queue.offer(next);
+            }
+        }
+    }
+    return result;
+}
