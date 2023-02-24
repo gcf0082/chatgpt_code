3815b88b66e9757f191882787f58a5be9ae50829
gcf0082 <13510989573@sina.cn>
2023-02-24 22:02:57 +0800

    Create FileDto.java

---
+++ html/使用bootstrap和datatable的文件搜索/后端分页/FileDto.java
@@ -0,0 +1,28 @@
+package com.example.filesearch.dto;
+
+public class FileDto {
+
+    private String fileName;
+    private String filePath;
+
+    public FileDto(String fileName, String filePath) {
+        this.fileName = fileName;
+        this.filePath = filePath;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+}
