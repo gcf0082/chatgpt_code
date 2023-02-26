fe7aa3ad0098a04967ccb17cd26882b7ef91d2e8
gcf0082 <13510989573@sina.cn>
2023-02-26 19:07:43 +0800

    Create config.groovy

---
+++ java/java使用groovy配置文件/config.groovy
@@ -0,0 +1,11 @@
+database {
+    driver = "com.mysql.jdbc.Driver"
+    url = "jdbc:mysql://localhost:3306/mydb"
+    username = "root"
+    password = "password"
+}
+
+server {
+    port = 8080
+    host = "localhost"
+}
