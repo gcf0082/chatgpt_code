09def9187034053b6652332ade0947205ae88491
gcf0082 <13510989573@sina.cn>
2023-02-27 20:25:07 +0800

    Create 封装okhttp发送请求的代码.java

---
+++ java/封装okhttp发送请求的代码.java
@@ -0,0 +1,25 @@
+import okhttp3.*;
+
+import java.io.IOException;
+
+public class OkHttpUtils {
+
+    private static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
+    private static OkHttpClient client = new OkHttpClient();
+
+    public static String sendJsonPost(String url, String json) throws IOException {
+        RequestBody body = RequestBody.create(JSON, json);
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        }
+    }
+
+    public static String sendJsonPost(String ip, int port, String json) throws IOException {
+        String url = "http://" + ip + ":" + port;
+        return sendJsonPost(url, json);
+    }
+}
